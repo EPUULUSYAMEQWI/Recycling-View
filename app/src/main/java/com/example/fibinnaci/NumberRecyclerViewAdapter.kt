49b64cf0f85cc8ae3c303fce3b6d578efc9e604b
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-06-29 22:36:30 +0300

    first commit

---
+++ app/src/main/java/com/example/fibinnaci/NumberRecyclerViewAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.fibinnaci
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NumberRecyclerViewAdapter (var numberList: List<Long>): RecyclerView.Adapter<NumbersViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  NumbersViewHolder {
+        var itemView = LayoutInflater .from(parent .context).inflate(R.layout.fibonacci_numbers , parent , false)
+        return NumbersViewHolder  (itemView)
+    }
+    override fun onBindViewHolder(holder: NumbersViewHolder,position: Int) {
+        holder.tvnumber.text = numberList.get(position).toString()
+    }
+    override fun getItemCount(): Int {
+        return numberList .size
+    }
+}
+class NumbersViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    var tvnumber = itemView.findViewById<TextView>(R.id.tvnumber )
+}
+
+
+
+
+
